1012c3911869083ee3cf8f836c0a7271938fa90c
Tanaka Bere <tanakabere@hotmail.co.uk>
2026-01-30 00:39:18 +0000

    Initial Commit

---
+++ TTMusic/TTMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTMusicApp.swift
+//  TTMusic
+//
+//  Created by Tanaka Bere on 30/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TTMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
